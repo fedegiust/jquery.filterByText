b464710ab656fa96875d0e4ae3d6d1e775f49176
Federico Giust <fgiustdj@gmail.com>
2017-01-27 09:21:21 -0300

    Create jquery.filterByText.js

---
+++ jquery.filterByText.js
@@ -0,0 +1,38 @@
+jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+    return this.each(function() {
+        var select = this;
+        var options = [];
+        $(select).find('option').each(function() {
+            options.push({
+                value: $(this).val(),
+                text: $(this).text(),
+                display: ($(this).css('display') || ""),
+                cssClass: ($(this).attr('class') || "")
+            });
+        });
+        $(select).data('options', options);
+        $(textbox).bind('change keyup', function() {
+            var options = $(select).empty().scrollTop(0).data('options');
+            var search = $.trim($(this).val());
+            var regex = new RegExp(search, 'gi');
+            $.each(options, function(i) {
+                var option = options[i];
+                if (option.text.match(regex) !== null) {
+                    if($('#displayAllFields').length){
+                        if($('#displayAllFields').attr('checked') === 'checked') {
+                            option.display = 'block';
+                            option.css = 'toggleOption';
+                        }
+                    }
+                    $(select).append(
+                        $('<option>').text(option.text).val(option.value).css('display', option.display).addClass(option.cssClass)
+                    );
+                }
+            });
+            if (selectSingleMatch === true &&
+                $(select).children().length === 1) {
+                $(select).children().get(0).selected = true;
+            }
+        });
+    });
+};
